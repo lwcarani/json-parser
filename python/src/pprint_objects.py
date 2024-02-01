44af74aee13208429be543da90738ffeb14ca556
Luke Carani <83194857+lwcarani@users.noreply.github.com>
2024-02-01 14:39:11 -0500

    Update pprint_objects.py

--- python/src/pprint_objects.py
+++ python/src/pprint_objects.py
@@ -8,18 +8,22 @@ def pprint_dict(d: dict, depth: int = 0) -> None:
             depth -= 1
         elif isinstance(v, list):
             depth += 1
-            pprint_list(v, depth)
+            pprint_list(L=v, is_in_array=False, depth=depth)
             depth -= 1
         elif isinstance(v, str):
-            print(f'"{v}"')
+            print(f'"{v}",')
         else:
-            print(v)
+            print(f"{v},")
     print(" " * depth * 4, end="")
-    print("}")
+    if depth == 0:
+        print("}")
+    else:
+        print("},")
 
 
-def pprint_list(L: list, depth: int = 0) -> None:
-    print(" " * depth * 4, end="")
+def pprint_list(L: list, is_in_array: bool, depth: int = 0) -> None:
+    if is_in_array:
+        print(" " * depth * 4, end="")
     print("[")
     for elem in L:
         if isinstance(elem, dict):
@@ -28,11 +32,11 @@ def pprint_list(L: list, depth: int = 0) -> None:
             depth -= 1
         elif isinstance(elem, list):
             depth += 1
-            pprint_list(elem, depth)
+            pprint_list(L=elem, is_in_array=True, depth=depth)
             depth -= 1
         elif isinstance(elem, str):
-            print(f'{" " * (depth + 1) * 4}"{elem}"')
+            print(f'{" " * (depth + 1) * 4}"{elem}",')
         else:
-            print(f'{" " * (depth + 1) * 4}{elem}')
+            print(f'{" " * (depth + 1) * 4}{elem},')
     print(" " * depth * 4, end="")
-    print("]")
+    print("],")
